193b0de027690d807a5e94feb5a0ba4977e64a08
seenso <>
2021-11-02 11:36:58 -0800

    Completed assignment; had a lot of trouble that ultimately came down to the fact that the return statement did not work in the .find() method. Had to create a win var outside .find() adn reassign the variable within .find(). Then returned win variable after .find()

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+let superbowlWin = (arrOfObj) => {
+  let win = undefined;
+  arrOfObj.find((play) => {
+    if (play.result === "W") {
+      console.log('WINNING PLAY-', play.result, play.year);
+      win = play.year; 
+    }
+  });
+  return win;
+};
