d9fc61e88daf2195d90211eb328696d1c2f9a536
seenso <>
2021-11-02 11:48:37 -0800

    commit tree 530de2d5fbd3446be8e48db05f95e7e435505adc

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let superbowlWin = (arrOfObj) => {
   let win = undefined;
   arrOfObj.find((play) => {
     if (play.result === "W") {
-      console.log('WINNING PLAY-', play.result, play.year);
+      // console.log('WINNING PLAY-', play.result, play.year);
       win = play.year; 
     }
   });
